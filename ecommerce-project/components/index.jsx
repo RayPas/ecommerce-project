142b2a4b6bee41855530d4471a7a7e273886211f
RayPas <70732899+RayPas@users.noreply.github.com>
2023-03-14 11:07:00 -0700

    updated

---
+++ ecommerce-project/components/index.jsx
@@ -0,0 +1,19 @@
+export { default as Footer } from './Footer'
+export { default as Layout } from './Layout'
+export { default as Navbar } from './Navbar'
+export { default as Product } from './Product'
+export { default as HeroBanner } from './HeroBanner'
+export { default as Cart } from './Cart'
+export { default as FooterBanner} from './FooterBanner'
+
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default index
